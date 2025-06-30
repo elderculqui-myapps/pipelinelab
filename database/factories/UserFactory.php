57d322328a63c96fb1a7ab117e76d77c1aae0149
ElderCulqui <elderculquig@gmail.com>
2025-06-30 14:48:31 -0500

    Fix user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,7 +25,7 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
-            'age' => rand(0, 100),
+            'age' => fake()->numberBetween(18, 99),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
